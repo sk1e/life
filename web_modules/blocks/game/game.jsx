223b71621e613a0700e0015fc0cc7be9dec28dae
Sergey Petrov <sekk1e@yandex.ru>
2016-12-15 22:38:33 +0700

    + grid

--- web_modules/blocks/game/game.jsx
+++ web_modules/blocks/game/game.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Grid from '../grid/grid.jsx';
 import './game.styl';
+
 
 class Game extends React.Component {
 
 
   render() {
     return (
-      <div className="game" />
+      <div className="game">
+        <Grid />
+      </div>
     );
   }
 }
---
+++ web_modules/blocks/grid/grid.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './grid.styl';
+
+
+class Grid extends React.Component {
+  render() {
+    return (
+      <div className="grid" />
+    );
+  }
+}
+
+
+export default Grid;
+
